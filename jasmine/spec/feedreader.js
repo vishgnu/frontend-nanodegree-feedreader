52159ce08468f5649a2b899eea231b8be35beaf3
vishgnu <niels.walter@gmail.com>
2016-04-21 20:13:49 +0200

    added async tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -18,14 +18,14 @@ OK Return the allFeeds variable to a passing state.
 OK Write a test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.
 OK Write a test that loops through each feed in the allFeeds object and ensures it has a name defined and that the name is not empty.
 OK Write a new test suite named "The menu".
-Write a test that ensures the menu element is hidden by default. You'll have to analyze the HTML and the CSS to determine how we're performing the hiding/showing of the menu element.
-Write a test that ensures the menu changes visibility when the menu icon is clicked. This test should have two expectations: does the menu display when clicked and does it hide when clicked again.
-Write a test suite named "Initial Entries".
-Write a test that ensures when the loadFeed function is called and completes its work, there is at least a single .entry element within the .feed container.
-Write a test suite named "New Feed Selection".
+OK Write a test that ensures the menu element is hidden by default. You'll have to analyze the HTML and the CSS to determine how we're performing the hiding/showing of the menu element.
+OK Write a test that ensures the menu changes visibility when the menu icon is clicked. This test should have two expectations: does the menu display when clicked and does it hide when clicked again.
+OK Write a test suite named "Initial Entries".
+OK Write a test that ensures when the loadFeed function is called and completes its work, there is at least a single .entry element within the .feed container.
+OK Write a test suite named "New Feed Selection".
 Write a test that ensures when a new feed is loaded by the loadFeed function that the content actually changes.
-No test should be dependent on the results of another.
-Callbacks should be used to ensure that feeds are loaded before they are tested.
+OK No test should be dependent on the results of another.
+OK Callbacks should be used to ensure that feeds are loaded before they are tested.
 Implement error handling for undefined variables and out-of-bound array access.
 When complete - all of your tests should pass.
 */
@@ -97,8 +97,6 @@ $(function() {
          * hiding/showing of the menu element.
          */
           it('element is hidden by default', function() {
-              
-              
               // get body element check for hidde-menu class
               var ishidden = $(document.body).hasClass( "menu-hidden");
               expect(ishidden).toBe(true);
@@ -112,14 +110,14 @@ $(function() {
             it('changes visibility when clicked', function() {
                 
             // click 
-            $(document.body).click();
+            $('a.menu-icon-link').click();
               
             //should show
-            var ishidden = $(document.body).hasClass("menu-hidden");
+            var ishidden = $('document.body').hasClass("menu-hidden");
             expect(ishidden).toBe(false);
 
             // click
-            $(document.body).click();
+            $('a.menu-icon-link').click();
              
             // should hide
              var ishidden = $(document.body).hasClass("menu-hidden");
@@ -137,15 +135,24 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+            var originalTimeout;
 
             beforeEach(function(done){
+                originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+                jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
                 loadFeed(0,done);
             });
 
             it("should have at least 1 .entry in the .feed container", function(done){
                 // Expect that there is something there in the initial load feed 
-                  expect($('.entriesLen').length).not.toBe(0);
-                done();
+                
+                  var entriesCount = $('.feed').children('.entry-link').length;
+                  expect(entriesCount).toBeGreaterThan(0);
+                  done();    
+            });
+            
+            afterEach(function() {
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
             });
 
         });
@@ -157,8 +164,32 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-         it('changing of feed will change content', function() {
-              expect(true).toBe(false);
+         var originalTimeout;
+         var initialContent;
+         
+         beforeEach(function(done){
+             
+                originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+                jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+             
+                // get initial content
+               initialContent =  $('.feed').html();
+               
+                loadFeed(1,done);
+            });
+        
+        
+         it('changing of feed will change content', function(done) {
+             
+             newContent = $('.feed').html();
+             // compare old and new html
+             expect(initialContent).not.toBe(newContent);
+             done();
           });
+          
+          afterEach(function() {
+           jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+          });
+
     });
 }());
